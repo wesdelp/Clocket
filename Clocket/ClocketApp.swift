e791d993fe7a9cf6047bf86aeb9e142310e1da68
Wes Delp <wesdelp@me.com>
2020-10-01 16:03:29 -0400

    Initial Commit

---
+++ Clocket/ClocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClocketApp.swift
+//  Clocket
+//
+//  Created by Wes Delp on 10/1/20.
+//
+
+import SwiftUI
+
+@main
+struct ClocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
